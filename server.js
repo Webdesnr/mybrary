07e06b11a1f9ee67f14d1b40958fca5201e80345
amar jothi <jamarjothi1@gmail.com>
2022-06-24 13:21:06 +0530

    initial author route

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const app = express();
 
 const expressLayouts = require("express-ejs-layouts");
 
+const authorsRouter = require("./routes/author");
 const indexRouter = require("./routes/index");
+
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 
@@ -28,5 +32,6 @@ db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connected to mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorsRouter);
 
 app.listen(process.env.PORT || 3000);
